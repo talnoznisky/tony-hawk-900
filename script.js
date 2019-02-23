7916bb292020e5fd881b6fd966f394dcd297df80
talnoznisky <tal.noznisky@gmail.com>
2019-02-23 17:42:34 -0600

    styling stable

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ window.addEventListener("keydown", turnTony)
 var initialDegree = 0;
 
 function degree(){
-  if(initialDegree <= 300){
+  if(initialDegree <= 900){
       return initialDegree++;
   }else{
     console.log("you're done!")
-    return 0;
+    return 900;
   }
 }
 function turnTony(){
-
   document.querySelector("img").style.transform =  `rotateY(${degree()}deg)`
 }
